9946dfbe6fa4732452faac86c6f2d7e74d335811
luca <luca@localhost.localdomain>
2020-05-29 19:23:26 +0200

    gradle tools to 4.0.0, minor fixes

--- lib/src/main/java/com/kirkbushman/araw/http/EnvelopedCommentData.kt
+++ lib/src/main/java/com/kirkbushman/araw/http/EnvelopedCommentData.kt
@@ -4,8 +4,10 @@ import com.kirkbushman.araw.http.base.EnvelopeKind
 import com.kirkbushman.araw.models.mixins.CommentData
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import kotlinx.android.parcel.Parcelize
 
 @JsonClass(generateAdapter = true)
+@Parcelize
 open class EnvelopedCommentData(
 
     @Json(name = "kind")
